1203e11caf579f9e0627716e819edae8ae0dab0b
Засеев Артур <a.zaseev@platformaofd.ru>
2023-06-01 11:13:41 +0300

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Стартовый шаблон Express + Handlebars
+
+## Установка
+- Устанавливаем зависимости `npm install`
+- Устанавливаем **browser-sync** глобально, для обновления браузера 
+при изменении файлов **handlebars** 
+  - `npm install -g browser-sync`
+
+## Запуск
+- Запуск **express** сервера `npm run dev`
+- Запуск **browser-sync** сервера `npm run ui`
+ 
---
+++ app.js
@@ -0,0 +1,25 @@
+import path from "path";
+import url from "url";
+import express from "express";
+import { engine } from "express-handlebars";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Setup
+const app = express();
+app.engine("handlebars", engine());
+
+// Settings
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get("/", (_, res) => {
+  res.render("index");
+});
+
+app.listen(3000, () => console.log("Server start jn port 3000"));
