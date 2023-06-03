607b76c0335161804ca7573c846dca94d1415e81
Засеев Артур <a.zaseev@platformaofd.ru>
2023-06-03 14:30:00 +0300

    bugfix

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/", (_, res) => {
-  res.render("index");
+router.get("/", (req, res) => {
+  res.render("index", {
+    user: req.user,
+  });
 });
 
 router.get("/profile", (req, res) => {
